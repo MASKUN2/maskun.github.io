b1c73c48fe750668043628d5ca9e15620d9d8034
masku <masku@MASKUNBOOK>
2023-05-12 09:35:50 +0900

    xptmxm2

--- src/pakage2/PerCalculator.java
+++ src/pakage2/PerCalculator.java
@@ -7,7 +7,7 @@ public class PerCalculator {
 		System.out.println("1. 주당시장가격 입력");
 		System.out.println("2. 주당 순이익 입력");
 		System.out.println("3. 계산결과 확인");
-		System.out.println("4. 종료");
+		System.out.println("4. 종료sd");
 	
 		
 
