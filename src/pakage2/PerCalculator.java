c3be33a6626ed4f087502eb889be00b6e615a6f6
masku <masku@MASKUNBOOK>
2023-05-12 18:14:46 +0900

    랩퍼클래스 boolean변경, 클래스 전역변수를 메소드 로컬변수화 시킴, 하위 메소드를 디폴트로변경, 계산메소드를 파라미터로 변경

--- src/pakage2/PerCalculator.java
+++ src/pakage2/PerCalculator.java
@@ -5,43 +5,27 @@ import java.util.Scanner;
 
 public class PerCalculator {
 
-	static Scanner sc = new Scanner(System.in);
-	static DecimalFormat Form = new DecimalFormat("###,###,####,###");
-	static DecimalFormat Form2 = new DecimalFormat("###,###,####,###.###");
-	//숫자 자릿수 변환 https://jamesdreaming.tistory.com/203
-	static Boolean LoopCheck = true;
-	static int Menu ;
-	
-	static int SharePrice = 0;
-	static int Earning = 0;
-	static int OutstandingShare = 0 ;
-	static double Eps = 0;
-	static double Per = 0;
-	
 	public static void main(String[] args) {
-		
-		
+		boolean LoopCheck = true;
+		int Menu ;
+		int SharePrice = 0;
+		double Eps = 0;
 		
 		while (LoopCheck) {
-			System.out.println("PER 계산기입니다. 번호를 입력하세요");
-			System.out.println("1. 주당시장가격 입력");
-			System.out.println("2. 주당 순이익 입력");
-			System.out.println("3. 계산결과 확인");
-			System.out.println("4. 종료");
-			Menu = sc.nextInt();
+			Menu = MenuSelect();
 			
 			switch (Menu) {
 			case 1: {
-				InputSharePrice();
+				SharePrice = InputSharePrice();
 				break;
 			}
 			
 			case 2: {
-				InputEps();								
+				Eps = InputEps();								
 				break;
 			}
 			case 3: {
-				CalPer();
+				CalPer(SharePrice, Eps);
 				break;
 			}
 			case 4: {
@@ -61,29 +45,61 @@ public class PerCalculator {
 			
 		}
 	
-		public static void InputSharePrice() {
+		static int MenuSelect() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("PER 계산기입니다. 번호를 입력하세요");
+			System.out.println("1. 주당시장가격 입력");
+			System.out.println("2. 주당 순이익 입력");
+			System.out.println("3. 계산결과 확인");
+			System.out.println("4. 종료");
+			int Menu = sc.nextInt();
+			return Menu;
+		}
+	
+		public static int InputSharePrice() {
+			int SharePrice = 0;
+			Scanner sc = new Scanner(System.in);
+			DecimalFormat Form = new DecimalFormat("###,###,####,###");
+			//숫자 자릿수 변환 https://jamesdreaming.tistory.com/203
 			System.out.println("1. 주당시장가격 입력");
 			System.out.println("현재주가를 입력해주세요(단위: 원)");
 			SharePrice = sc.nextInt();
 			System.out.println(Form.format(SharePrice));
 			System.out.printf("입력된 값은 %s 원입니다. %n%n", Form.format(SharePrice));
+			return SharePrice;
 		}
 		
-		public static void InputEps() {
+		public static double InputEps() {
+			int Earning = 0;
+			int OutstandingShare = 0 ;
+			double Eps = 0;
+			Scanner sc = new Scanner(System.in);
+			DecimalFormat Form = new DecimalFormat("###,###,####,###");
+			DecimalFormat Form2 = new DecimalFormat("###,###,####,###.###");
+			//숫자 자릿수 변환 https://jamesdreaming.tistory.com/203
+			
 			System.out.println("2. 주당 순이익 입력");
 			System.out.println("당기순이익을 입력해주세요(단위: 원)");
 			Earning = sc.nextInt();
-			System.out.printf("입력된 값은 %s 원입니다. %n%n", Form.format(Earning));
 			
+			System.out.printf("입력된 값은 %s 원입니다. %n%n", Form.format(Earning));
 			System.out.println("유통주식수를 입력해주세요(단위: 원)");
 			OutstandingShare = sc.nextInt();
+			
 			System.out.printf("입력된 값은 %s 입니다. %n%n",Form.format(OutstandingShare));
 			Eps = Earning / (double)OutstandingShare;
 			System.out.printf("계산된 주당 순이익은 %s 원 입니다. %n %n", Form2.format(Eps));
-
+			
+			return Eps;
 		}
-		public static void CalPer() {
+		
+		public static void CalPer(int SharePrice, double Eps) {
+			DecimalFormat Form2 = new DecimalFormat("###,###,####,###.###");
+			//숫자 자릿수 변환 https://jamesdreaming.tistory.com/203
+			double Per = 0;
+			
 			Per = SharePrice / Eps;
+			
 			System.out.println("3. 계산결과 확인");
 			System.out.printf("PER은 %s 배 입니다. %n%n", Form2.format(Per));
 			
